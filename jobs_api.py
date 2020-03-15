fba6b2c24e3ca1267ac97dd2599180f3737a6e28
N1k0lay78 <kolya.toropof@gmail.com>
2020-03-15 11:23:59 +0300

    сделано: API для получения работы

--- jobs_api.py
+++ jobs_api.py
@@ -16,8 +16,24 @@ def get_jobs():
         {
             'jobs':
                 [job.to_dict(only=(
+                    'id', 'team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished',
+                    'user.name'))
+                    for job in jobs]
+        }
+    )
+
+
+@blueprint.route('/api/jobs/<int:job_id>')
+def get_one_jobs(job_id):
+    session = db_session.create_session()
+    job = session.query(Jobs).get(job_id)
+    if not job:
+        return jsonify({'error': 'Not found'})
+    return jsonify(
+        {
+            'jobs': job.to_dict(only=(
                 'id', 'team_leader', 'job', 'work_size', 'collaborators', 'start_date', 'end_date', 'is_finished',
                 'user.name'))
-                 for job in jobs]
+
         }
     )
